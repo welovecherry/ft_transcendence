76401b9c751491e2d6b3a0d45686f43aae78685d
Sumin Lee <suminn0707@gmail.com>
2024-12-30 23:48:32 +0900

    feat: postEnrollment api를 listener에서 분리해서 eventHandler에서 처리하도록 수정

--- project_root/frontend/js/core/event.js
+++ project_root/frontend/js/core/event.js
@@ -3,9 +3,10 @@ import { gameSettings, renderGameOptions } from '../pages/settingOptions.js';
 import { renderSubgameMenu } from '../pages/subgame/subgameDisplay.js';
 import { startMatch, showResult } from '../pages/subgame/subgameMatch.js';
 import { attachEventListeners } from '../pages/subgame/subgameButtonSelect.js';
+import { postEnrollment } from '../api/enroll.js';
 
 // click 이벤트 발생 시 유형에 따라 처리
-export function eventHandler(event) {
+export async function eventHandler(event) {
     const target = event.target;
 
     if (target.matches('[data-action="navigateTo"]')) {
@@ -34,7 +35,15 @@ export function eventHandler(event) {
         const selectedRadio = document.querySelector(
             'input[name="btnradio"]:checked'
         );
-        console.log(selectedRadio);
+        let data = {
+            id: '',
+            select: '',
+        };
+        data.id = 1;
+        data.select = selectedRadio.getAttribute('id');
+
+        const response = await postEnrollment(data);
+        renderSubgameMenu('enroll');
     }
 
     if (target.matches('[data-action="subgameStart"]')) {
@@ -45,7 +54,7 @@ export function eventHandler(event) {
         showResult();
     }
 
-    if (target.matches('[data-action="showHistory"]')) {
-        //
-    }
+    // if (target.matches('[data-action="showHistory"]')) {
+    //     //
+    // }
 }
--- project_root/frontend/js/pages/subgame/subgameButtonSelect.js
+++ project_root/frontend/js/pages/subgame/subgameButtonSelect.js
@@ -1,28 +1,45 @@
+import { postEnrollment } from '../../api/enroll.js';
+import { postMatchResult } from '../../api/match.js';
+import { renderSubgameMenu } from './subgameDisplay.js';
+
 export function attachEventListeners() {
     const radioButtons = document.querySelectorAll('input[name="btnradio"]');
     const saveButton = document.getElementById('save-button');
 
-    radioButtons.forEach((radio) => {
-        radio.addEventListener('change', () => {
-            saveButton.disabled = !Array.from(radioButtons).some(
-                (btn) => btn.checked
-            );
+    if (radioButtons) {
+        radioButtons.forEach((radio) => {
+            radio.addEventListener('change', () => {
+                saveButton.disabled = !Array.from(radioButtons).some(
+                    (btn) => btn.checked
+                );
+            });
         });
-    });
+    }
 
-    let enrollData = {
-        me_id: '',
-        me_select: '',
-    };
+    // if (saveButton) {
+    //     saveButton.addEventListener('click', async () => {
+    // const selectedRadio = document.querySelector(
+    //     'input[name="btnradio"]:checked'
+    // );
+    // if (mode === 'enroll') {
+    //     let data = {
+    //         id: '',
+    //         select: '',
+    //     };
+    //     data.id = 1;
+    //     data.select = selectedRadio.getAttribute('id');
 
-    saveButton.addEventListener('click', async () => {
-        const selectedRadio = document.querySelector(
-            'input[name="btnradio"]:checked'
-        );
-        enrollData.me_id = 1;
-        enrollData.me_select = selectedRadio.getAttribute('id');
-        const data = await postEnrollment(enrollData);
-
-        renderSubgameMenu('enroll');
-    });
+    //     const response = await postEnrollment(data);
+    //     renderSubgameMenu(mode);
+    //         } else if (mode === 'match') {
+    //             let data = {
+    //                 id: '',
+    //                 select: '',
+    //             };
+    //             data.id = 1;
+    //             data.select = selectedRadio.getAttribute('id');
+    //             const response = await postMatchResult(data);
+    //         }
+    //     });
+    // }
 }
