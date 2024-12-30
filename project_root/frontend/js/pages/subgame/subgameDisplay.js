cea3fcd01886ad4f2db822f5e85429a5870f1f78
Sumin Lee <suminn0707@gmail.com>
2024-12-30 23:50:07 +0900

    refactor: 가위바위보 버튼 이름 수정 및 주석 정리

--- project_root/frontend/js/pages/subgame/subgameDisplay.js
+++ project_root/frontend/js/pages/subgame/subgameDisplay.js
@@ -1,34 +1,34 @@
-import { getEnrollment, postEnrollment } from '../../api/enroll.js';
-// import { attachEventListeners } from './subgameButtonSelect.js';
+import { getEnrollment } from '../../api/enroll.js';
 
 export const renderSubgameMenu = async (mode) => {
     let subgameHTML = '';
 
     if (mode === 'enroll') {
-        //api 호출 -> 지금 내가 정보 갖고 있는지 보기
         const data = await getEnrollment();
         console.log(data);
         if (data && data.select) {
             subgameHTML = `
-				<p>You've already enrolled: ${data.select}</p>
-				<p>Wait for your opponent...</p>
+				<div class="container mb-5">
+					<p>You've already enrolled: ${data.select}</p>
+					<p>Wait for your opponent...</p>
+				</div>
+			`;
+        } else {
+            subgameHTML = `
+				<p>Enroll your choice</p>
+				<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+					<input type="radio" class="btn-check" name="btnradio" id="Rock" autocomplete="off">
+					<label class="btn btn-outline-primary" for="Rock">Rock</label>
+	
+					<input type="radio" class="btn-check" name="btnradio" id="Scissors" autocomplete="off">
+					<label class="btn btn-outline-primary" for="Scissors">Scissors</label>
+	
+					<input type="radio" class="btn-check" name="btnradio" id="Paper" autocomplete="off">
+					<label class="btn btn-outline-primary" for="Paper">Paper</label>
+				</div>
+				<button class="btn btn-success" id="save-button" data-action="subgameSave" disabled>Save</button>
 			`;
         }
-
-        subgameHTML += `
-			<p>Update your choice</p>
-			<div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-				<input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off">
-				<label class="btn btn-outline-primary" for="btnradio1">Rock</label>
-
-				<input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-				<label class="btn btn-outline-primary" for="btnradio2">Scissors</label>
-
-				<input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-				<label class="btn btn-outline-primary" for="btnradio3">Paper</label>
-			</div>
-			<button class="btn btn-success" id="save-button" data-action="subgameSave" disabled>Save</button>
-		`;
     } else if (mode === 'match') {
         subgameHTML = `
 			<button class="btn btn-success" data-action="subgameStart">Start!</button>
@@ -37,26 +37,4 @@ export const renderSubgameMenu = async (mode) => {
 
     document.getElementById('subgame-content').innerHTML = subgameHTML;
     document.getElementById('subgame-match').innerHTML = '';
-
-    // attachEventListeners();
-    // postAndRefreshListner();
 };
-
-// function postAndRefreshListner() {
-//     const saveButton = document.getElementById('save-button');
-//     let enrollData = {
-//         me_id: '',
-//         me_select: '',
-//     };
-
-//     saveButton.addEventListener('click', async () => {
-//         const selectedRadio = document.querySelector(
-//             'input[name="btnradio"]:checked'
-//         );
-//         enrollData.me_id = 1;
-//         enrollData.me_select = selectedRadio.getAttribute('id');
-//         const data = await postEnrollment(enrollData);
-
-//         renderSubgameMenu('enroll');
-//     });
-// }
