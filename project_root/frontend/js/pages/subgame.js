efea08143bace6a31a4a8aad6440599c81eb528f
Sumin Lee <suminn0707@gmail.com>
2024-12-30 23:49:23 +0900

    feat: getHistory api 구현

--- project_root/frontend/js/pages/subgame.js
+++ project_root/frontend/js/pages/subgame.js
@@ -1,4 +1,7 @@
-export function render() {
+import { getHistory } from '../api/history.js';
+import { didWin } from './subgame/subgameDidWin.js';
+
+export async function render() {
     const content = document.getElementById('content');
     content.innerHTML = '';
     content.innerHTML = `
@@ -13,4 +16,29 @@ export function render() {
             <div id="subgame-history" data-action="showHistory"></div>
         </div>
     `;
+
+    const data = await getHistory();
+
+    let totalCount = 0;
+    let winCount = 0;
+    let user_id = 1;
+
+    data.forEach((match) => {
+        totalCount++;
+        const me_id = match.me_id;
+        const me_select = match.me_select;
+        const other_id = match.other_id;
+        const other_select = match.other_select;
+
+        if (me_id === user_id && didWin(me_select, other_select) === 1) {
+            winCount++;
+        }
+        if (other_id === user_id && didWin(other_select, me_select) === 1) {
+            winCount++;
+        }
+    });
+
+    const history = document.getElementById('subgame-history');
+    history.innerHTML = `<p>Total count: ${totalCount}</p>
+        <p>Win count: ${winCount}</p>`;
 }
