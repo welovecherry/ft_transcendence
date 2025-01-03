392896ffe178c2c5b3efdd1401540e974bf4b6a1
Sumin Lee <suminn0707@gmail.com>
2025-01-03 21:13:55 +0900

    feat: 잘못된 링크일 경우 기본 홈으로 리다이렉트하도록 구현

--- project_root/frontend/js/core/router.js
+++ project_root/frontend/js/core/router.js
@@ -11,14 +11,14 @@ const routes = {
 async function renderPage(pageName) {
     let pageModule;
     if (!isAuthenticated && pageName != 'login') {
-        console.log(isAuthenticated);
-        console.log(pageName);
-        console.log('login');
+        console.log(1);
         pageModule = await import(`../pages/login.js`);
+    } else if (isAuthenticated && pageName === 'login') {
+        console.log(2);
+        pageModule = await import(`../pages/setting.js`);
     } else {
         console.log(isAuthenticated);
         console.log(pageName);
-        console.log('pagename');
         pageModule = await import(`../pages/${pageName}.js`);
     }
     pageModule.render();
@@ -26,9 +26,16 @@ async function renderPage(pageName) {
 
 // URL 이동 처리
 export function navigateTo(path) {
-    window.history.pushState({}, path, window.location.origin + path);
+    const validPaths = Object.keys(routes);
     const pageName = routes[path] || 'login';
-    renderPage(pageName);
+
+    if (!validPaths.includes(path)) {
+        window.history.pushState({}, '', '/');
+        renderPage('login');
+    } else {
+        window.history.pushState({}, '', path);
+        renderPage(pageName);
+    }
 }
 
 // 브라우저의 뒤로가기/앞으로가기 이벤트 처리
