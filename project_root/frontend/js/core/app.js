b1d6985dd4d251aecc2ae05f03feaba4d30a5937
Sumin Lee <suminn0707@gmail.com>
2025-01-05 20:23:46 +0900

    fix: setting 거치지 않고 playing 접속시도 방어 및 playing 이탈 시 세팅 리셋

--- project_root/frontend/js/core/app.js
+++ project_root/frontend/js/core/app.js
@@ -2,9 +2,23 @@ import { navigateTo, handlePopState } from './router.js';
 import { eventHandler } from './event.js';
 import { handleOAuthCallback } from './auth.js';
 
+function resetSettings() {
+    localStorage.removeItem('gameSettings');
+}
+
 export function initializeApp() {
     // 브라우저 히스토리 이벤트 감지
     window.addEventListener('popstate', handlePopState);
+    window.addEventListener('popstate', (event) => {
+        if (event.state && event.state.page === 'playing') {
+            console.log('playing');
+        } else {
+            console.log('stopped playing. setting reset');
+            resetSettings();
+        }
+    });
+
+    // 페이지 내 클릭 이벤트 감지
     document.body.addEventListener('click', eventHandler);
 
     // 페이지 로드 시 callback 처리
