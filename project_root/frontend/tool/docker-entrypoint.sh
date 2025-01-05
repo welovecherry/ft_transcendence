cf4614d88ed9a18c3e72d635663370113ba290d4
Sumin Lee <suminn0707@gmail.com>
2025-01-05 20:10:04 +0900

    comment: https 접속을 위한 설정 주석으로 추가

---
+++ project_root/frontend/tool/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+# -keyout $CERT_KEY -out $CERT \
+# -subj "/C=KR/ST=Seoul/L=a/O=b/OU=c/CN=d"
+
+# envsubst '$CERT $CERT_KEY $DOMAIN_NAME' < /etc/nginx/conf.d/conf.conf > /etc/nginx/conf.d/conf.tmp
+# mv /etc/nginx/conf.d/conf.tmp /etc/nginx/conf.d/conf.conf
+
+exec "$@"
