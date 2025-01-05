1f218d919be98b2568d8a2cec4deea8310f37652
Sumin Lee <suminn0707@gmail.com>
2025-01-05 23:08:57 +0900

    feat: 로그인 여부에 맞게 프론트 화면 라우팅 구현

--- project_root/frontend/js/core/router.js
+++ project_root/frontend/js/core/router.js
@@ -1,4 +1,4 @@
-import { isAuthenticated } from './auth.js';
+import { isAuthenticated } from '../api/oauth.js';
 
 const routes = {
     '/': 'login',
@@ -9,29 +9,26 @@ const routes = {
 
 // 동적 페이지 렌더링
 async function renderPage(pageName) {
-    let pageModule;
-    if (!isAuthenticated && pageName != 'login') {
-        console.log(1);
-        pageModule = await import(`../pages/login.js`);
-    } else if (isAuthenticated && pageName === 'login') {
-        console.log(2);
-        pageModule = await import(`../pages/setting.js`);
-    } else {
-        console.log(isAuthenticated);
-        console.log(pageName);
-        pageModule = await import(`../pages/${pageName}.js`);
-    }
+    const pageModule = await import(`../pages/${pageName}.js`);
     pageModule.render();
 }
 
 // URL 이동 처리
-export function navigateTo(path) {
+export async function navigateTo(path) {
     const validPaths = Object.keys(routes);
     const pageName = routes[path] || 'login';
+    const isLogin = await isAuthenticated();
+    console.log(isLogin);
 
-    if (!validPaths.includes(path)) {
+    if (!isLogin && (!validPaths.includes(path) || pageName != 'login')) {
         window.history.pushState({}, '', '/');
         renderPage('login');
+    } else if (
+        isLogin &&
+        (!validPaths.includes(path) || pageName === 'login')
+    ) {
+        window.history.pushState({}, '', '/setting');
+        renderPage('setting');
     } else {
         window.history.pushState({}, '', path);
         renderPage(pageName);
